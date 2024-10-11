812793669a1382e0ecc5bf68f95cdc795d5dd2dd
Tanay Kumar <155322101+acode-42@users.noreply.github.com>
2024-10-11 13:34:21 +0530

    Selection_Sort_C++.cpp

--- Selection_Sort_C++.cpp
+++ Selection_Sort_C++.cpp
@@ -1,57 +1,62 @@
-//Implementing Selection Sort on an array 'arr' of Size 'n'
+// Implementing Selection Sort on an array 'arr' of Size 'n'
 
-
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
+void selectionSort(int arr[], int n)
+{
+    int temp, mini, i, j;
 
-void selectionSort(int arr[], int n){
-    int temp,mini,i,j;
-
-    for(i=0;i<=n-2;i++){
-        mini=i;
-        for(j=i;j<=n-1;j++){
-            if(arr[j]<arr[mini]){
-                mini=j;
+    for (i = 0; i <= n - 2; i++)
+    {
+        mini = i;
+        for (j = i; j <= n - 1; j++)
+        {
+            if (arr[j] < arr[mini])
+            {
+                mini = j;
             }
         }
 
-        temp=arr[mini];
-        arr[mini]=arr[i];
-        arr[i]=temp;
+        temp = arr[mini];
+        arr[mini] = arr[i];
+        arr[i] = temp;
     }
 }
 
-void printList(int arr[], int n){
-    for (int i = 0; i < n; i++){
+void printList(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         cout << arr[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 }
 
-
-int main(){
+int main()
+{
     int size;
 
-    cout<<"Enter size of the array:";
-    cin>>size;
+    cout << "Enter size of the array:";
+    cin >> size;
 
     int arr[size];
 
-    cout<<"Enter in array:\n";
-    for(int i=0;i<size;i++){
-        cin>>arr[i];
+    cout << "Enter in array:\n";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
     }
 
-    cout<<"Before sorting:\n";
-    printList(arr,size);
+    cout << "Before sorting:\n";
+    printList(arr, size);
 
-    selectionSort(arr,size);
+    selectionSort(arr, size);
 
-    cout<<"After Sorting:\n";
+    cout << "After Sorting:\n";
 
-    printList(arr,size);
+    printList(arr, size);
 
     return 0;
 }
