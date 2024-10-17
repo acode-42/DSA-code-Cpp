f7683ec97f2db4ca775a64ed239b6a68f65d7f5b
Tanay Kumar <155322101+acode-42@users.noreply.github.com>
2024-10-17 13:38:51 +0530

    Add files via upload

---
+++ move_zeroes_in end.cpp
@@ -0,0 +1,48 @@
+// move all the zeroes to end of the array
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> moveZero(vector<int> arr, int n)
+{
+    int j = -1;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == 0)
+        {
+            j=i;
+            break;
+        }
+    }
+
+    if (j == -1)
+    {
+        return arr;
+    }
+
+    for (int i = j + 1; i < n; i++)
+    {
+        if (arr[i] != 0)
+        {
+            swap(arr[i], arr[j]);
+            j++;
+        }
+    }
+
+    return arr;
+}
+
+int main()
+{
+    vector<int> arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+    int n = 10;
+    vector<int> ans = moveZero(arr, n);
+    for (auto &it : ans)
+    {
+        cout << it << " ";
+    }
+    cout << '\n';
+    return 0;
+}
